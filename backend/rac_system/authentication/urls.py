64e349e9ce69cb6028c8df84cc7a491c479ba1be
singhanmolpreet <anmoljosan18@gmail.com>
2024-11-16 13:51:38 +0530

    Added Email Verification

--- backend/rac_system/authentication/urls.py
+++ backend/rac_system/authentication/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.SignupPage,name='signup'),
     path('login/',views.LoginPage,name='login'),
-    path('logout/', views.LogoutPage,name='logout')
+    path('logout/', views.LogoutPage,name='logout'),
+    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
 ]
    
