5fb60087cc2f94aee52ea2b9f7579d427d713306
singhanmolpreet <anmoljosan18@gmail.com>
2025-02-16 16:17:03 +0530

    Home Page and Recruitment Section

--- backend/Chemovate_Society/Chemovate_Society/settings.py
+++ backend/Chemovate_Society/Chemovate_Society/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'Chemovate_Society.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
